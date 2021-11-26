442f2cb5ee3a8e0e1253c8956aba742d1abd58af
Ana VR <varaty7@gmail.com>
2021-11-25 20:44:32 -0600

    Quizz mini game

---
+++ sketch.js
@@ -0,0 +1,30 @@
+let canvas, backgroundImage;
+
+let gameState = 0;
+let contestantCount;
+let allContestants;
+let answer;
+let database;
+
+let question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
